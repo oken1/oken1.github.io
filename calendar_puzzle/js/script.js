e134124df8150cd92bdfe0e394d291063c72f5bf
oken1 <8463012+oken1@users.noreply.github.com>
2021-05-26 19:42:40 +0900

    calendar puzzle add

---
+++ calendar_puzzle/js/script.js
@@ -0,0 +1,33 @@
+$( document ).ready( () => {
+	const grid = 32
+
+	$( '.parts' ).each( ( index, element ) => {
+		const rx = Math.floor( Math.random() * 24 )
+		const ry = Math.floor( Math.random() * 16 )
+
+		$( element ).css( { left: rx * grid + 'px', top: ry * grid + 'px' } )
+		$( element ).attr( 'rotate', 0 )
+		$( element ).attr( 'scale', 1 )
+	} ).draggable(
+		{
+			grid: [grid, grid]
+		}
+	).on( 'dblclick', ( event ) => {
+		let r = parseInt( $( event.currentTarget ).attr( 'rotate' ), 10 )
+		let s = parseInt( $( event.currentTarget ).attr( 'scale' ), 10 )
+
+		r = ( r == 270 ) ? 0 : r + 90
+
+		$( event.currentTarget ).attr( 'rotate', r )
+			.css( { transform: `scale(${s}, 1) rotate(${r}deg)` } )
+	} ).on( 'contextmenu', ( event ) => {
+		let r = parseInt( $( event.currentTarget ).attr( 'rotate' ), 10 )
+		let s = parseInt( $( event.currentTarget ).attr( 'scale' ), 10 )
+		s = ( s == 1 ) ? -1 : 1
+	
+		$( event.currentTarget ).attr( 'scale', s )
+			.css( { transform: `scale(${s}, 1) rotate(${r}deg)` } )
+
+		return false
+	})
+} )
